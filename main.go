165b9dfb94983e40030b0f1587abbdc0fcc21d46
Isay Konter <kwekkwekpatu@Isays-Laptop.local>
2024-07-30 21:23:18 +0200

    Update main Go application file\n\n- Modify the main.go file

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
+
+type apiConfig struct {
+	mu             sync.Mutex
+	fileserverHits int
+}
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", readinessHandler)
+	apiCfg := apiConfig{}
+	mux.Handle("/app/*", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", readinessHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.middlewareMetricsResult)
+	mux.HandleFunc("/reset", apiCfg.middlewareMetricsReset)
+
 	server := &http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
@@ -19,4 +29,28 @@ func main() {
 func readinessHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.mu.Lock()
+		defer cfg.mu.Unlock()
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsResult(writer http.ResponseWriter, request *http.Request) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	body := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	writer.WriteHeader(200)
+	writer.Write([]byte(body))
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(writer http.ResponseWriter, request *http.Request) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	cfg.fileserverHits = 0
+	writer.WriteHeader(200)
 }
